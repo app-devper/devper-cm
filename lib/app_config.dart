c01d06f477976390bee68ee7bf7f97590cb07ab1
Worawit <worawit.bs@gmail.com>
2022-05-20 13:48:54 +0700

    Common - add app config

---
+++ lib/app_config.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+class AppConfig {
+  final String apiUrl;
+  final String logo;
+  final String name;
+  final String system;
+
+  AppConfig({
+    required this.apiUrl,
+    required this.logo,
+    required this.name,
+    required this.system,
+  });
+
+  static Future<AppConfig> forEnvironment(String? env) async {
+    // set default to dev if nothing was passed
+    env = env ?? 'dev';
+
+    // load the json file
+    final contents = await rootBundle.loadString(
+      'config/$env.json',
+    );
+
+    // decode our json
+    final json = jsonDecode(contents);
+
+    // convert our JSON into an instance of our AppConfig class
+    return AppConfig(
+      apiUrl: json['apiUrl'],
+      logo: json['logo'],
+      name: json['name'],
+      system: json['system'],
+    );
+  }
+}
