d3aed79665c8b836b4b15786cb79e39dd5c5c1d8
Worawit <worawit.bs@gmail.com>
2022-05-20 14:05:50 +0700

    Common - edit widget

--- lib/core/widget/button_widget.dart
+++ lib/core/widget/button_widget.dart
@@ -1,6 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:common/theme.dart';
 
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: CustomColor.primary,
+        minimumSize: const Size.fromHeight(50),
+      ),
+      child: FittedBox(
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 16, color: Colors.white),
+        ),
+      ),
+      onPressed: onClicked,
+    );
+  }
+}
+
+class ButtonIconWidget extends StatelessWidget {
+  final Icon icon;
+  final VoidCallback onClicked;
+
+  const ButtonIconWidget({
+    Key? key,
+    required this.icon,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: CustomColor.primary,
+        minimumSize: const Size.fromHeight(50),
+      ),
+      child: FittedBox(
+        child: icon,
+      ),
+      onPressed: onClicked,
+    );
+  }
+}
+
 class ButtonPrimary extends StatelessWidget {
   final String text;
   final VoidCallback onClicked;
