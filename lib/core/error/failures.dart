c2a22a0c0662b6f7d415d6722ce73028c0d7fc14
Worawit <worawit.bs@gmail.com>
2023-07-05 20:37:53 +0700

    feat: edit failure

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -5,16 +5,21 @@ import 'package:common/data/network/exception.dart';
 
 class Failure {
   late String error;
+  late String errorCode;
 
   Failure(Exception e) {
     if (e is AppException) {
+      errorCode = "A-500";
       error = e.error;
     } else if (e is HttpException) {
       Map<String, dynamic> json = jsonDecode(e.response.body);
+      errorCode = "N-${e.response.statusCode}";
       error = json['error'] ?? e.response.reasonPhrase;
     } else if (e is SocketException) {
+      errorCode = "N-000";
       error = "Connection error. Please try again.";
     } else {
+      errorCode = "A-000";
       error = e.toString();
     }
   }
